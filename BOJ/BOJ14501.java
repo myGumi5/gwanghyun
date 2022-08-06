4a44a2dc2d1c44f98abfe6ee6155b84e6e4990a9
성광현 <gch03944@gmail.com>
2022-08-06 16:05:25 +0900

    2022-08-06

---
+++ BOJ/BOJ14501.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ14501 {
+	
+	public static void main(String[] args) {
+		FastIn sc = new FastIn();
+		
+		int N = sc.nextInt();
+		int[] dp = new int[N+2];
+		int T = 0;
+		int P = 0;
+		int max = -1;
+		for (int i=1;i<N+1;i++) {
+			T = sc.nextInt();
+			P = sc.nextInt();
+			dp[i] = Math.max(dp[i-1], dp[i]);
+			if(i+T>N+1)
+				continue;
+			dp[i + T] = Math.max(dp[i] + P, dp[i+T]);
+			max = Math.max(dp[i+T], max);
+		}
+		System.out.println(max);
+	}
+	
+	
+	
+	
+	static class FastIn {
+		BufferedReader br;
+		StringTokenizer st;
+		
+		public FastIn() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		
+		String next() {
+			
+			while(st ==null||!st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+	}
+}
