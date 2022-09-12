5461ba8020e247183eb683a59a9d566fc6e7c193
성광현 <gch03944@gmail.com>
2022-09-12 21:52:41 +0900

    2022-09-12

---
+++ BOJ/BOJ4999.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class BOJ4999 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		if(sc.next().length()>=sc.next().length()) {
+			System.out.println("go");
+		} else {
+			System.out.println("no");
+		}
+
+	}
+
+}
